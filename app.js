71d7c2d8c41fcfb1772685f3bd8c4c1b96c2c7ba
jhkim <jhkim@jhkim.local>
2019-06-20 14:54:19 +0900

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var supertest = require("supertest");
 const should = require('chai').should();
 var server = supertest.agent("http://localhost:9671");
-const assert = require('assert');  // Node.js `assert` module
+const assert = require('assert'); // Node.js `assert` module
 const conn = require('./data/connection.json')
 
 module.exports = {
@@ -17,31 +17,35 @@ module.exports = {
   b: function () {
     return 'B!';
   },
-  get: function(done, data) {
-     server
+  get: function (done, data) {
+    // let contentType = data.contentType === undefined ? /json/ : data.contentType;
+    server
       .get(data.url)
       .expect("Content-type",/json/)
+      // .expect("Content-type", contentType)
       .expect(200) // THis is HTTP response
-      .end(function(err,res){
-        should.equal(err, null); 
+      .end(function (err, res) {
+        should.equal(err, null);
         res.status.should.equal(200);
         // console.log('JSON.stringify', JSON.stringify(res.body));
         assert.equal(JSON.stringify(res.body), JSON.stringify(data.res));
         done();
       });
   },
-  post: function(done, data) {
+  post: function (done, data) {
+    // let contentType = data.contentType === undefined ? /json/ : data.contentType;
     server
-     .post(data.url)
-     .send(data.req)
-     .expect("Content-type",/json/)
-     .expect(200) // THis is HTTP response
-     .end(function(err,res){
-       should.equal(err, null); 
-       res.status.should.equal(200);
-       // console.log('JSON.stringify', JSON.stringify(res.body));
-       assert.equal(JSON.stringify(res.body), JSON.stringify(data.res));
-       done();
-     });
+      .post(data.url)
+      .send(data.req)
+       .expect("Content-type",/json/)
+      // .expect("Content-type", contentType)
+      .expect(200) // THis is HTTP response
+      .end(function (err, res) {
+        should.equal(err, null);
+        res.status.should.equal(200);
+        // console.log('JSON.stringify', JSON.stringify(res.body));
+        assert.equal(JSON.stringify(res.body), JSON.stringify(data.res));
+        done();
+      });
   }
 }
--- test/agapi.spec.js
+++ test/agapi.spec.js
@@ -4,16 +4,34 @@ const app = require('../app');
 // var server = supertest.agent("http://localhost:9671");
 // const assert = require('assert');  // Node.js `assert` module
 
-const conn = require('../data/connection.json')
+// const conn = require('../data/connection.json')
+// const conn = require('../data/sequence.json')
+const conn = require('../data/schema.json')
 
-let use = !true;
+let use = true;
 let req, res;
-let describeName = 'Connection';
-let itName = 'RemoveConnection';
+// let describeName = 'Connection';
+// let describeName = 'Sequence';
+let describeName = 'Schema';
+
+
 // let itName = 'GetConnectionList';
-req = {
-  "ConnectionName": "test conneciton name"
-};
+// let itName = 'AddConnection';
+// let itName = 'TestConnection';
+// let itName = 'RemoveConnection';
+// let itName = 'GetSequences';
+// let itName = 'CreateSequence';
+
+let itName = 'GetSchemaList (AgensGarph)';
+// let itName = 'RemoveSchema (odbc)';
+// let itName = 'RemoveSchema (AgensGarph - vertex)';
+// let itName = 'RemoveSchema (AgensGarph - edge)';
+
+// req = {
+//   // "ConnectionName": "test connection name"
+//   // "ConnectionName": "agens graph connection"
+//   "ConnectionName": "pq"
+// };
 res = {
   "Success": true
 }
